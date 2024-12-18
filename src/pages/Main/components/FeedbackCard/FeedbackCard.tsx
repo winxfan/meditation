30ec167eb2006a691269649581fb072acb619af2
Slava Khripushin <slava.cpa@yandex.ru>
2024-12-18 22:00:21 +0700

    fix FeedbackCard

--- src/pages/Main/components/FeedbackCard/FeedbackCard.tsx
+++ src/pages/Main/components/FeedbackCard/FeedbackCard.tsx
@@ -1,24 +1,16 @@
-import css from './FeedbackCard.module.scss';
+import css from "./FeedbackCard.module.scss";
 
 export const FeedbackCard = () => {
-
-	return (
-		<div className={css.container}>
-			<div>
-				<p className={css.title}>отзыв</p>
-			</div>
-
-			<div className={css.body}>
-				<p className={css.title}>
-					Каждый месяц вы будете получать новое звание внутри платформы и уникальными скетч картинками отражающими ваш прогресс
-				</p>
-			</div>
-
-			<div className={css.footer}>
-				<button className={css.readMore}>
-					Читать полностью
-				</button>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className={css.container}>
+      <p className={css.title}>отзыв</p>
+      <p className={css.text}>
+        Каждый месяц вы будете получать новое звание внутри платформы и
+        уникальными скетч картинками отражающими ваш прогресс. Каждый месяц вы
+        будете получать новое звание внутри платформы и уникальными скетч
+        картинками отражающими ваш прогресс
+      </p>
+      <button className={css.readMore}>Читать полностью</button>
+    </div>
+  );
+};
