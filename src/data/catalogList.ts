99d89980c02b4f829af3105a74999cf2c0db204a
Slava Khripushin <slava.cpa@yandex.ru>
2024-12-18 19:42:03 +0700

    catalog training link

---
+++ src/data/catalogList.ts
@@ -0,0 +1,59 @@
+import image1 from "@/assets/image/catalogItem-1.jpg";
+import image2 from "@/assets/image/catalogItem-2.jpg";
+import image3 from "@/assets/image/catalogItem-3.jpg";
+import image4 from "@/assets/image/catalogItem-4.jpg";
+import image5 from "@/assets/image/catalogItem-5.jpg";
+import image6 from "@/assets/image/catalogItem-6.jpg";
+import image7 from "@/assets/image/catalogItem-7.jpg";
+import image8 from "@/assets/image/catalogItem-8.jpg";
+
+export const catalogList = [
+  {
+    color: "white",
+    date: "29 октября",
+    title: "Жиросжигающая тренировки",
+    image: image1,
+  },
+  {
+    color: "black",
+    date: "29 октября",
+    title: "Тренировка на все тело",
+    image: image2,
+  },
+  {
+    color: "white",
+    date: "29 октября",
+    title: "Функциональная тренировка",
+    image: image3,
+  },
+  {
+    color: "white",
+    date: "29 октября",
+    title: "Тренировка на все тело",
+    image: image4,
+  },
+  {
+    color: "black",
+    date: "29 октября",
+    title: "Функциональная тренировка",
+    image: image5,
+  },
+  {
+    color: "black",
+    date: "29 октября",
+    title: "Тренировка на все тело",
+    image: image6,
+  },
+  {
+    color: "white",
+    date: "29 октября",
+    title: "Жиросжигающая тренировки",
+    image: image7,
+  },
+  {
+    color: "black",
+    date: "29 октября",
+    title: "Тренировка на все группы мышц",
+    image: image8,
+  },
+];
