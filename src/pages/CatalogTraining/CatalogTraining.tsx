1fe51c60e0bd9edd5606952f389bbf0f55afaafd
Slava Khripushin <slava.cpa@yandex.ru>
2024-12-18 19:42:03 +0700

    catalog training link

--- src/pages/CatalogTraining/CatalogTraining.tsx
+++ src/pages/CatalogTraining/CatalogTraining.tsx
@@ -1,16 +1,9 @@
 import { Header } from "@/components/Header";
-import css from './CatalogTraining.module.scss';
-import {FixedSubscribeBtn} from "@/components/FixedSubscribeBtn";
+import css from "./CatalogTraining.module.scss";
+import { FixedSubscribeBtn } from "@/components/FixedSubscribeBtn";
 import { CatalogItem } from "./components/CatalogItem";
 
-import image1 from '@/assets/image/catalogItem-1.jpg'
-import image2 from '@/assets/image/catalogItem-2.jpg'
-import image3 from '@/assets/image/catalogItem-3.jpg'
-import image4 from '@/assets/image/catalogItem-4.jpg'
-import image5 from '@/assets/image/catalogItem-5.jpg'
-import image6 from '@/assets/image/catalogItem-6.jpg'
-import image7 from '@/assets/image/catalogItem-7.jpg'
-import image8 from '@/assets/image/catalogItem-8.jpg'
+import { catalogList } from "@/data/catalogList";
 
 export const CatalogTraining = () => {
   return (
@@ -21,17 +14,21 @@ export const CatalogTraining = () => {
         <h2 className={css.title}>Каталог тренировок</h2>
 
         <div className={css.grid}>
-            <CatalogItem color="white" date="29 октября" title="Жиросжигающая тренировки" image={image1} />
-            <CatalogItem color="black" date="29 октября" title="Тренировка на все тело" image={image2} />
-            <CatalogItem color="white" date="29 октября" title="Функциональная тренировка" image={image3} />
-            <CatalogItem color="white" date="29 октября" title="Тренировка на все тело" image={image4} />
-            <CatalogItem color="black" date="29 октября" title="Функциональная тренировка" image={image5} />
-            <CatalogItem color="black" date="29 октября" title="Тренировка на все тело" image={image6} />
-            <CatalogItem color="white" date="29 октября" title="Жиросжигающая тренировки" image={image7} />
-            <CatalogItem color="black" date="29 октября" title="Тренировка на все группы мышц" image={image8} />
+          {catalogList.map((item, i) => {
+            return (
+              <CatalogItem
+                key={i}
+                link="single"
+                color={item.color}
+                date={item.date}
+                title={item.title}
+                image={item.image}
+              />
+            );
+          })}
         </div>
 
-        <FixedSubscribeBtn/>
+        <FixedSubscribeBtn />
       </div>
     </div>
   );
--- src/pages/CatalogTraining/components/CatalogItem/CatalogItem.tsx
+++ src/pages/CatalogTraining/components/CatalogItem/CatalogItem.tsx
@@ -1,19 +1,31 @@
-import css from './CatalogItem.module.scss';
+import css from "./CatalogItem.module.scss";
+import { Link } from "react-router-dom";
 
 interface CatalogItemProps {
-    title: string;
-    date: string;
-    image: string;
-    color?: 'white' | 'black';
+  title: string;
+  date: string;
+  image: string;
+  color: string;
+  link: string;
 }
 
-export const CatalogItem: React.FC<CatalogItemProps> = ({ title, date, image, color }) => {
-    const itemClass = color ? `${css.item} ${css[color]}` : css.item;
+export const CatalogItem: React.FC<CatalogItemProps> = ({
+  title,
+  date,
+  image,
+  color,
+  link,
+}) => {
+  const itemClass = color ? `${css.item} ${css[color]}` : css.item;
 
-    return (
-      <div className={itemClass} style={{backgroundImage: `url(${image})`}}>
-        <span className={css.item__date}>{date}</span>
-        <h4 className={css.item__title}>{title}</h4>
-      </div>
-    );
+  return (
+    <Link
+      to={link}
+      className={itemClass}
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      <span className={css.item__date}>{date}</span>
+      <h4 className={css.item__title}>{title}</h4>
+    </Link>
+  );
 };
