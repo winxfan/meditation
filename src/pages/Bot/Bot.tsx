932f1884bd296c16b7516ccd5911df458cf30631
Danil <92051977+danil-rezbaev@users.noreply.github.com>
2024-12-20 22:44:54 +0200

    update

--- src/pages/Bot/Bot.tsx
+++ src/pages/Bot/Bot.tsx
@@ -1,21 +1,41 @@
-import cl from './Bot.module.scss';
+import css from './Bot.module.scss';
 import RoundBlock from '@/components/RoundBlock';
-import { Typography } from 'antd';
 import {FixedSubscribeBtn} from "@/components/FixedSubscribeBtn";
 import banner from "@/assets/image/PageBannerBot.png";
 import {useBackButton} from "@/utils/hooks/useBackButton";
 import communityScreen from "@/assets/image/CommunityScreen.png";
-import css from "@/pages/Community/Community.module.scss";
 
 export const PageBot = () => {
 	useBackButton();
 
 	return (
-		<div className={cl.page_bot}>
-			<img src={banner} className={cl.banner} />
+		<div className={css.page_bot}>
+			<div
+				className={css.banner}
+				style={{backgroundImage: `url(${banner})`}}
+			>
+				<h1 className={css.bannerTitle}>
+					Бот, который будет сопровождать вас на протяжении всего года
+				</h1>
 
-			<RoundBlock classNames={cl.bot_short_descr} background="#EFEDF0">
-				<p className={cl.bot_short_descr__text}>
+				<ul className={css.bannerSubtitle}>
+					<li>
+						Утром присылает ежедневную практику
+					</li>
+					<li>
+						Днем присылает короткие задания и сообщения осознанности
+					</li>
+					<li>
+						Вечер – форма саморефлексии
+					</li>
+					<li>
+						3 раза в неделю просит отчет по вашим целям
+					</li>
+				</ul>
+			</div>
+
+			<RoundBlock classNames={css.bot_short_descr} background="#EFEDF0">
+				<p className={css.bot_short_descr__text}>
 					Каждый месяц вы&nbsp;будете ставить себе новые цели в&nbsp;бот, над которыми
 					будете работать в&nbsp;рамках месяца. Вы&nbsp;сможете всегда находиться
 					в&nbsp;фокусе на&nbsp;главных задачах, чтобы каждый месяц, без выгорания
