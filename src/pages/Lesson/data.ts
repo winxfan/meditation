d7bbe09f987b89a3feec4d643fbc215401f0e28e
Danil <92051977+danil-rezbaev@users.noreply.github.com>
2024-12-21 09:45:50 +0200

    fix

--- src/pages/Lesson/data.ts
+++ src/pages/Lesson/data.ts
@@ -2,10 +2,6 @@ import banner1 from '@/assets/image/Lesson1.png'
 import banner2 from '@/assets/image/Lesson2.png'
 import banner3 from '@/assets/image/Lesson3.png'
 import banner4 from '@/assets/image/Lesson4.png'
-import audio1 from './data/practice1.mp3'
-import audio2 from './data/practice2.mp3'
-import audio3 from './data/practice3.mp3'
-import audio4 from './data/practice4.mp3'
 
 export const lessonData = [
 	{
@@ -14,7 +10,7 @@ export const lessonData = [
 		homeworkDescription: 'Напишите, что вы почувствовали после прохождения практика на уровне эмоций и на уровне эмоций? Какие осознания к вам пришли во время урока и выполнения практики?',
 		videoId: '/379dff1d532919da0bfba7a354bf3238/?p=gE9Sxdqq-c9IW8QHgxVPng',
 		audioTitle: "Пробуждение внутренней силы",
-		audioLink: audio1
+		audioLink: 'https://storage.yandexcloud.net/puzzlecourse/audio/practice1.mp3'
 	},
 	{
 		id: 2,
@@ -22,7 +18,7 @@ export const lessonData = [
 		homeworkDescription: 'Напишите, что вы почувствовали после прохождения практика на уровне эмоций и на уровне эмоций? Какие осознания к вам пришли во время урока и выполнения практики?',
 		videoId: '/507f8935834a8ed00c6c194559cd81f5/?p=bDUes3bhAlbjQOYoqwl1iw',
 		audioTitle: "Трансформация боли в ресурс",
-		audioLink: audio2
+		audioLink: 'https://storage.yandexcloud.net/puzzlecourse/audio/practice2.mp3'
 	},
 	{
 		id: 3,
@@ -30,7 +26,7 @@ export const lessonData = [
 		homeworkDescription: 'Напишите, что вы почувствовали после прохождения практика на уровне эмоций и на уровне эмоций? Какие осознания к вам пришли во время урока и выполнения практики?',
 		videoId: '/8b4b4628546d1ece1944066ab5da2433/?p=xph9dnPTArFsXpDFyb8stg',
 		audioTitle: "Освобождение от обид и травм прошлого",
-		audioLink: audio3
+		audioLink: 'https://storage.yandexcloud.net/puzzlecourse/audio/practice3.mp3'
 	},
 	{
 		id: 4,
@@ -38,6 +34,6 @@ export const lessonData = [
 		homeworkDescription: 'Напишите детально, что вы почувствовали после прохождения практика на уровне тела и эмоций? Какие осознания к вам пришли во время урока и выполнения практики?',
 		videoId: '/2d8b87c56a8e8a5fa44540aa5d882cc8/?p=yiiGosoAZf0qz05-sty9Vg',
 		audioTitle: "Шаг в новую реальность",
-		audioLink: audio4
+		audioLink: 'https://storage.yandexcloud.net/puzzlecourse/audio/practice4.mp3'
 	},
 ]
