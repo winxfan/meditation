0cad180138195f0476b02040f27ee44da8bd667d
Danil <92051977+danil-rezbaev@users.noreply.github.com>
2024-12-21 11:02:48 +0200

    fix

--- src/pages/Lesson/data.ts
+++ src/pages/Lesson/data.ts
@@ -7,7 +7,7 @@ export const lessonData = [
 	{
 		id: 1,
 		pictureSrc: banner1,
-		homeworkDescription: 'Напишите, что вы почувствовали после прохождения практика на уровне эмоций и на уровне эмоций? Какие осознания к вам пришли во время урока и выполнения практики?',
+		homeworkDescription: 'Напишите детально, что вы почувствовали после прохождения практика на уровне тела и эмоций? Какие осознания к вам пришли во время урока и выполнения практики?',
 		videoId: '/379dff1d532919da0bfba7a354bf3238/?p=gE9Sxdqq-c9IW8QHgxVPng',
 		audioTitle: "Пробуждение внутренней силы",
 		audioLink: 'https://storage.yandexcloud.net/puzzlecourse/audio/practice1.mp3'
@@ -15,7 +15,7 @@ export const lessonData = [
 	{
 		id: 2,
 		pictureSrc: banner2,
-		homeworkDescription: 'Напишите, что вы почувствовали после прохождения практика на уровне эмоций и на уровне эмоций? Какие осознания к вам пришли во время урока и выполнения практики?',
+		homeworkDescription: 'Напишите детально, что вы почувствовали после прохождения практика на уровне тела и эмоций? Какие осознания к вам пришли во время урока и выполнения практики?',
 		videoId: '/507f8935834a8ed00c6c194559cd81f5/?p=bDUes3bhAlbjQOYoqwl1iw',
 		audioTitle: "Трансформация боли в ресурс",
 		audioLink: 'https://storage.yandexcloud.net/puzzlecourse/audio/practice2.mp3'
@@ -23,7 +23,7 @@ export const lessonData = [
 	{
 		id: 3,
 		pictureSrc: banner3,
-		homeworkDescription: 'Напишите, что вы почувствовали после прохождения практика на уровне эмоций и на уровне эмоций? Какие осознания к вам пришли во время урока и выполнения практики?',
+		homeworkDescription: 'Напишите детально, что вы почувствовали после прохождения практика на уровне тела и эмоций? Какие осознания к вам пришли во время урока и выполнения практики?',
 		videoId: '/8b4b4628546d1ece1944066ab5da2433/?p=xph9dnPTArFsXpDFyb8stg',
 		audioTitle: "Освобождение от обид и травм прошлого",
 		audioLink: 'https://storage.yandexcloud.net/puzzlecourse/audio/practice3.mp3'
